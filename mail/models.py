94ee33b12a96ecb54f7d7c2cc9069af1870b3221
wicked-smart <marzaanvan@gmail.com>
2023-01-20 19:51:28 +0530

    update gitignore

--- mail/models.py
+++ mail/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass 
+
+class Email(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="emails")
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="emails_sent")
+    reciever = models.ManyToManyField(User, related_name="emails_recieved")
+    subject = models.CharField(max_length=256)
+    body = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+    archive = models.BooleanField(default=False)
+
+    
--- project3_cs50/settings.py
+++ project3_cs50/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mail',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -83,6 +84,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'mail.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
