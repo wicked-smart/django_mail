b989faab857983102233099201b501a92b874949
wicked-smart <marzaanvan@gmail.com>
2023-01-31 16:58:21 +0530

    Add urls

--- project3_cs50/urls.py
+++ project3_cs50/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mail/', include('mail.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
