4619206b189f81aa0bf59bc768a3f15c6993c08b
wicked-smart <marzaanvan@gmail.com>
2023-09-28 22:27:15 +0530

    test cmiit

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
 import subprocess
-import os
 
-user_id = os.environ.get("USER_ID")
+celery_cmd = ["celery", "-A", "mail", "worker", "--loglevel=info", "--uid=1000"]
+subprocess.run(celery_cmd, check=True, cwd="/app")
 
-# Convert user_id to an integer
-try:
-    user_id = int(user_id)
-except (ValueError, TypeError):
-    user_id = None  # Handle the case where user_id is not a valid integer
-
-if user_id is not None:
-    celery_cmd = ["celery", "-A", "mail", "worker", "--loglevel=info", "--uid=1000"]
-    subprocess.run(celery_cmd, check=True, cwd="/app")
-else:
-    print("Invalid or missing USER_ID environment variable.")
