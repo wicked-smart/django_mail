9867a0953786926cf40db118912610c8dedb2999
wicked-smart <marzaanvan@gmail.com>
2023-10-21 12:32:55 +0530

    Add update

--- Procfile
+++ Procfile
@@ -1,7 +1,6 @@
 web: gunicorn project3_cs50.wsgi 
 worker: celery -A mail worker --loglevel=info
-release: python3 manage.py makemigrations && \
-         python3 manage.py migrate && \
-         python3 manage.py collectstatic --no-input
+release: python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py collectstatic --no-input
+         
+         
 
-##
--- project3_cs50/settings.py
+++ project3_cs50/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 import secrets
 import dj_database_url
@@ -35,6 +35,10 @@ SECRET_KEY = os.environ.get(
 #testing
 
 IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
+
+if not IS_HEROKU_APP:
+    load_dotenv(BASE_DIR / '.env')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not IS_HEROKU_APP:
@@ -93,19 +97,22 @@ WSGI_APPLICATION = 'project3_cs50.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if IS_HEROKU_APP:
+#if IS_HEROKU_APP:
     # In production on Heroku the database configuration is derived from the `DATABASE_URL`
     # environment variable by the dj-database-url package. `DATABASE_URL` will be set
     # automatically by Heroku when a database addon is attached to your Heroku app. See:
     # https://devcenter.heroku.com/articles/provisioning-heroku-postgres
     # https://github.com/jazzband/dj-database-url
-    DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        ),
-    }
+DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASES = {
+    "default": dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True,
+    ),
+}
+'''
 else:
     # When running locally in development or in CI, a sqlite database file will be used instead
     # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
@@ -115,6 +122,9 @@ else:
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
+
+
+'''
 
 
 '''
